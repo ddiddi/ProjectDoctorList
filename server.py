903410a592a64d30cc9df46e8435655867fc95c6
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2019-06-23 16:27:29 -0700

    Add menu filling from backend

--- server.py
+++ server.py
@@ -4,11 +4,12 @@ from flask import Flask, render_template
 # create the application object
 app = Flask(__name__)
 
+doctor_names = ["Julius Herbbert", "Algernop Keiger", "Nick Riviero"]
 # use decorators to link the function to a url
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Home")  # render a template
+    return render_template('index.html', title="Home", doctors = doctor_names)  # render a template
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
